2ff74e563137e1315441d1cb93f6af9d2c9b96d3
Roberto R. Expósito <rreye@udc.es>
2021-10-18 17:19:52 +0200

    Update to 2021/2022

--- Vagrantfile
+++ Vagrantfile
@@ -7,7 +7,9 @@ BASEIP="192.168.100"
 MASTERIP=10
 
 Vagrant.configure("2") do |config|
-  config.vm.box = "hashicorp/bionic64"
+  config.vm.box = "ubuntu/focal64"
+  config.vm.box_version = "20210917.0.0"
+  config.vm.box_check_update = false
 
   # Master node
   config.vm.define "master", primary: true do |master|
@@ -15,15 +17,17 @@ Vagrant.configure("2") do |config|
     master.vm.network :private_network, ip: "#{BASEIP}.#{MASTERIP}"
 
     master.vm.provider :virtualbox do |prov|
-        prov.cpus = "1"
-        prov.memory = "1024"
+	prov.name = "ICAP-P4-Master"
+        prov.cpus = 1
+        prov.memory = 1024
+	prov.linked_clone = true
 
         for i in 0..1 do
-            filename = "./disks/#{master.vm.hostname}-disk#{i}.vmdk"
+            filename = "disks/#{master.vm.hostname}-disk#{i}.vdi"
             unless File.exist?(filename)
-                prov.customize ["createmedium", "disk", "--filename", filename, "--size", 5 * 1024]
-                prov.customize ["storageattach", :id, "--storagectl", "SATA Controller", "--port", i + 1, "--device", 0, "--type", "hdd", "--medium", filename]
+                prov.customize ["createmedium", "disk", "--filename", filename, "--format", "vdi", "--size", 5 * 1024]
             end
+	    prov.customize ["storageattach", :id, "--storagectl", "SCSI", "--port", i + 3, "--device", 0, "--type", "hdd", "--medium", filename]
         end
     end
   end
@@ -35,22 +39,24 @@ Vagrant.configure("2") do |config|
         worker.vm.network :private_network, ip: "#{BASEIP}.#{i + MASTERIP}"
         
         worker.vm.provider :virtualbox do |prov|
+	    prov.name = "ICAP-P4-Worker#{i}"
             prov.cpus = 1
             prov.memory = WORKER_MEM
+	    prov.linked_clone = true
 
-            for i in 0..1 do
-                filename = "./disks/#{worker.vm.hostname}-disk#{i}.vmdk"
+            for j in 0..1 do
+                filename = "disks/#{worker.vm.hostname}-disk#{j}.vdi"
                 unless File.exist?(filename)
-                    prov.customize ["createmedium", "disk", "--filename", filename, "--size", 5 * 1024]
-                    prov.customize ["storageattach", :id, "--storagectl", "SATA Controller", "--port", i + 1, "--device", 0, "--type", "hdd", "--medium", filename]
+                    prov.customize ["createmedium", "disk", "--filename", filename, "--format", "vdi", "--size", 5 * 1024]
                 end
+                prov.customize ["storageattach", :id, "--storagectl", "SCSI", "--port", j + 3, "--device", 0, "--type", "hdd", "--medium", filename]
             end
         end
     end
   end
   
   # Global provisioning bash script
-  config.vm.provision "shell", path: "./bootstrap.sh" do |script|
+  config.vm.provision "shell", path: "provisioning/bootstrap.sh" do |script|
       script.args = [NUM_WORKERS, BASEIP, MASTERIP]
   end
 end
--- provisioning/bootstrap.sh
+++ provisioning/bootstrap.sh
@@ -2,7 +2,7 @@
 
 if [ "$#" -ne 3 ]; then
     echo "Sintaxis: $0 NUM_WORKERS BASEIP MASTERIP"
-    exit
+    exit -1
 fi
 
 NUM_WORKERS=$1
@@ -12,29 +12,40 @@ MASTERIP=$3
 # Format and mount disks to be used with Hadoop HDFS
 if [ ! -d "/data/disk0" ]; then
     mkdir -p /data/disk0 >& /dev/null
-    mkfs.ext4 -F /dev/sdb
-    mount /dev/sdb /data/disk0
+    mkfs.ext4 -F /dev/sdc
+    mount /dev/sdc /data/disk0
     chmod 1777 /data/disk0
 else
-    mount /dev/sdb /data/disk0 >& /dev/null
+    mount /dev/sdc /data/disk0 >& /dev/null
     chmod 1777 /data/disk0
 fi
 
 if [ ! -d "/data/disk1" ]; then
     mkdir -p /data/disk1 >& /dev/null
-    mkfs.ext4 -F /dev/sdc
-    mount /dev/sdc /data/disk1
+    mkfs.ext4 -F /dev/sdd
+    mount /dev/sdd /data/disk1
     chmod 1777 /data/disk1
 else
-    mount /dev/sdc /data/disk1 >& /dev/null
+    mount /dev/sdd /data/disk1 >& /dev/null
     chmod 1777 /data/disk1
 fi
 
-if ! grep -Fq /dev/sdb /etc/fstab ; then
-    echo -e "/dev/sdb        /data/disk0     ext4    defaults,relatime       0       0" >> /etc/fstab
+if [ ! -d "/data/disk0/hdfs" ]; then
+    mkdir /data/disk0/hdfs
 fi
+
+if [ ! -d "/data/disk1/hdfs" ]; then
+    mkdir /data/disk1/hdfs
+fi
+
+chmod 1777 /data/disk0/hdfs
+chmod 1777 /data/disk1/hdfs
+
 if ! grep -Fq /dev/sdc /etc/fstab ; then
-    echo -e "/dev/sdc        /data/disk1     ext4    defaults,relatime       0       0" >> /etc/fstab
+    echo -e "/dev/sdc        /data/disk0     ext4    defaults,relatime       0       0" >> /etc/fstab
+fi
+if ! grep -Fq /dev/sdd /etc/fstab ; then
+    echo -e "/dev/sdd        /data/disk1     ext4    defaults,relatime       0       0" >> /etc/fstab
 fi
 
 apt-get update
@@ -71,27 +82,52 @@ if ! grep -Fq JAVA_HOME /home/vagrant/.profile ; then
     echo "export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64" >> /home/vagrant/.profile
 fi
 
-# NFS setup
+# NFS and SSH keys setup
+SSH_PUBLIC_KEY=/vagrant/provisioning/id_rsa.pub
+SSH_DIR=/home/vagrant/.ssh
+
 if [ ! -d "/share" ]; then
     mkdir /share >& /dev/null
 fi
     
 if [ "$HOSTNAME" = "master" ]; then
-    cp /vagrant/setupSSH.sh /home/vagrant
-    dos2unix ./setupSSH.sh
-    chown vagrant:vagrant ./setupSSH.sh
-    chmod +x ./setupSSH.sh
+    # Create ssh keys
+    echo -e 'y\n' | sudo -u vagrant ssh-keygen -t rsa -f $SSH_DIR/id_rsa -q -N ''
 
+    if [ ! -f $SSH_DIR/id_rsa.pub ]; then
+        echo "SSH public key could not be created"
+        exit -1
+    fi
+
+    chown vagrant:vagrant $SSH_DIR/id_rsa*
+    cp $SSH_DIR/id_rsa.pub /vagrant/provisioning
+
+    # NFS export
     chmod 1777 /share
 
     if ! grep -Fq /share /etc/exports ; then
         echo -e "/share        $BASEIP.0/24(rw,sync,no_subtree_check)" >> /etc/exports
     fi
+
     exportfs -a
 else
-    umount /share >& /dev/null
+    umount /share >& /dev/null && sleep 1
+
     if ! grep -Fq /share /etc/fstab ; then
         echo -e "master:/share        /share     nfs    auto,relatime,tcp       0       0" >> /etc/fstab
     fi
-    mount /share
+
+    sleep 1 && mount /share
 fi
+
+# Finish SSH keys setup
+if [ ! -f $SSH_PUBLIC_KEY ]; then
+	echo "SSH public key does not exist"
+	exit -1
+fi
+
+sed -i "/master/d" .ssh/authorized_keys >& /dev/null
+cat $SSH_PUBLIC_KEY >> $SSH_DIR/authorized_keys
+chown vagrant:vagrant $SSH_DIR/authorized_keys
+chmod 0600 $SSH_DIR/authorized_keys >& /dev/null
+
